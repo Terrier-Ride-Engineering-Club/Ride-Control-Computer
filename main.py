727d2639a6a26a99807678fbb5e2975606db435a
Jack Justus <jackjustushome@gmail.com>
2025-02-23 15:38:09 -0500

    Requirements

--- main.py
+++ main.py
@@ -4,11 +4,16 @@
 r'''
 Notes for members:
     INITIAL SETUP:
-    1. Naviate to your project directory:
+    1. Make sure you have the most recent version of python installed
+        python3 --version => make sure it says 3.13.2
+    2. Naviate to your project directory:
         cd path\to\your\project
-    2. Make sure you have created a virtual environment by running:
+    3. Make sure you have created a virtual environment by running:
         python3 -m venv venv
-    3. Install required dependicies by running:
+    4. Activate the virtual environment by running:
+        [MacOS/Linux]: source venv/bin/activate
+        [Windows]: venv\Scripts\activate
+    5. Install required dependicies by running:
         pip install -r requirements.txt
 
     TO RUN PROJECT:
@@ -22,6 +27,7 @@ Notes for members:
 # Native modules
 import threading
 import logging
+from roboclaw import RoboClaw
 # Custom modules
 from ridecontrolcomputer import RideControlComputer
 
