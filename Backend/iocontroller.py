5eb43a19a6083b5751a9533e0859ba71bc1fd5d2
Jack Justus <jackjustushome@gmail.com>
2025-03-07 00:34:43 -0500

    Update iocontroller.py

--- Backend/iocontroller.py
+++ Backend/iocontroller.py
@@ -5,6 +5,7 @@ import platform
 import logging
 from gpiozero import Device
 from gpiozero.pins.native import NativeFactory
+from gpiozero.pins.lgpio import LGPIOFactory
 
 # Use MockFactory for non-Raspberry Pi platforms.
 if platform.system() != 'Linux':
@@ -12,7 +13,7 @@ if platform.system() != 'Linux':
     Device.pin_factory = MockFactory()
     logging.getLogger('IOController').warning(f'Current platform [{platform.system()}] ≠ Linux. IO Running in mock mode')
 else:
-    Device.pin_factory = NativeFactory()
+    Device.pin_factory = LGPIOFactory(chip=4)
 
 from gpiozero import Button
 import threading
