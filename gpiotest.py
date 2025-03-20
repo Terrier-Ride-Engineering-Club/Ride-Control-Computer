678fabc6b9b8b9d0693944274dcaa205c7e472da
Jack Justus <jackjustushome@gmail.com>
2025-03-19 23:48:07 -0400

    Update gpiotest.py

--- gpiotest.py
+++ gpiotest.py
@@ -1,7 +1,9 @@
-from gpiozero.pins.native import NativeFactory
+# from gpiozero.pins.native import NativeFactory
+from gpiozero.pins.lgpio import LGPIOFactory
 from gpiozero import Device, LED
 
-Device.pin_factory = NativeFactory()
+# Device.pin_factory = NativeFactory()
+Device.pin_factory = LGPIOFactory()
 
 # These will now implicitly use NativePin instead of RPiGPIOPin
 led1 = LED(16)
