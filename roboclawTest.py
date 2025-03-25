7bd52c5b3b7cd02806a300c053289e9eb788662c
Jack Justus <jackjustushome@gmail.com>
2025-03-25 15:57:08 -0400

    Create roboclawTest.py

---
+++ roboclawTest.py
@@ -0,0 +1,24 @@
+"""
+GPIO14 - UART_TX
+GPIO15 - UART_RX
+
+RaspiConfig Serial Tutorial: https://resources.basicmicro.com/configuring-the-raspberry-pi-3-serial-port/
+RoboClaw tutorial: https://resources.basicmicro.com/packet-serial-with-the-raspberry-pi-3/
+"""
+
+
+from roboclaw import RoboClaw
+from time import sleep
+
+if __name__ == "__main__":
+    
+    address = 0x80
+    roboclaw = RoboClaw("/dev/ttyS0", 2400)
+    
+    while True:
+        
+        roboclaw.drive_motor(1,10)
+        sleep(2)
+        roboclaw.drive_motor(1,0)
+        sleep(2)
+        
