65a2d2ccde2e35e063960be39d62c99db0f8b7c2
Jack Justus <jackjustushome@gmail.com>
2025-03-26 14:41:39 -0400

    Update roboclawTest.py

--- roboclawTest.py
+++ roboclawTest.py
@@ -17,8 +17,10 @@ if __name__ == "__main__":
     
     while True:
         
-        print(roboclaw.read_status())
+        print(f"STATUS: {roboclaw.read_status()}")
+        print(f"VOLTAGE: {roboclaw.read_batt_voltage('main')}")
         sleep(2)
+        
         # roboclaw.drive_motor(1,0)
         # sleep(2)
         
