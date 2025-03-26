c786c6b5b35bbfb5bd021ef99c9840441536812d
Jack Justus <jackjustushome@gmail.com>
2025-03-26 19:23:30 -0400

    Update roboclawTest.py

--- roboclawTest.py
+++ roboclawTest.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     address = 0x80
     mc = RoboClaw(port='/dev/ttyAMA0',address=0x80)
     
-    MODE = "DIAGNOSTIC"
+    MODE = "MOTOR TEST"
     while True:
         if MODE == "DIAGNOSTIC":
             print(f"VER: {mc.read_version()}")
