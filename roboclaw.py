94303f405e9d4f6b3ce42fcafbef98a2588ab167
Jack Justus <jackjustushome@gmail.com>
2025-03-26 17:21:25 -0400

    Update roboclaw.py

--- roboclaw.py
+++ roboclaw.py
@@ -32,7 +32,7 @@ class RoboClaw:
     def _read(self, cmd, fmt):
         cmd_bytes = struct.pack('>BB', self.address, cmd)
         try:
-            # self.port.reset_input_buffer()  # TODO: potential bug?
+            self.port.reset_input_buffer()  # TODO: potential bug?
             with self.serial_lock:
                 print(f"WRITE: {cmd_bytes.hex()}", end=" == ")
                 self.port.write(cmd_bytes)
