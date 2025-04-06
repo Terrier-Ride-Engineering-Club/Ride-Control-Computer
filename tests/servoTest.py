c0d8dfb7677881fd3f9455ccf60843e1df03fc66
Jack Justus <jackjustushome@gmail.com>
2025-04-06 15:15:21 -0400

    Update servoTest.py

--- tests/servoTest.py
+++ tests/servoTest.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     io = HardwareIOController()
 
     while True:
+        print('OUT')
         io.extend_servos()
         time.sleep(2)
+        print('IN')
         io.retract_servos()
         time.sleep(2)
 
